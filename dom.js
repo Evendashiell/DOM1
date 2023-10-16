09e2b7094b228a253c3444bec8a82c843b5adea6
Evendashiell <slowsteady0412@gmail.com>
2023-10-16 23:39:50 +0530

    DOM1

---
+++ dom.js
@@ -0,0 +1,5 @@
+var itemm = document.getElementsByClassName('itemm');
+console.log(itemm);
+itemm[0].textContent = 'Hello';
+var li = document.getElementsByTagName('li');
+li[4].textContent = 'Bye';
